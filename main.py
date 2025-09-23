ea68f720314b39a87b80af45119d90733ba50c63
Priestest Alexander <priestest.alexander@gmail.com>
2025-09-22 20:22:42 -0400

    Image button added that attaches an image to the canvas when the button is triggered. Need to think about preserving the original size of the image, so that the use image isn't alter by size but instead by just appears smaller on the screen so they could see the watermark properly.
    Keep thinking an accessing the the different scope and flow in program. it is vital!

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 from PIL import Image, ImageDraw, ImageFont,ImageTk
-from tkinter import *
+from tkinter import Scale,HORIZONTAL
 import tkinter as tk
 from tkinter import filedialog
 import customtkinter
-    
+  
+FILE_NAME = None
+pil_image = None
+tk_image = None
+
+def choose_image():
+    global FILE_NAME,pil_image,tk_image
+    FILE_NAME = filedialog.askopenfilename(initialdir = "/",
+                                          title = "Select Your Image File",
+                                          filetypes = (("PNG Files","*.png*"),
+                                                       ("JPEG Files","*.jpg*")
+                                                        ))
+    if FILE_NAME:
+        original_image = Image.open(FILE_NAME)
+        pil_image = original_image.resize((400, 400), Image.Resampling.LANCZOS)
+
+        tk_image = ImageTk.PhotoImage(pil_image)
+        canvas.create_image(100, 100, image=tk_image, anchor=tk.CENTER)
+
+
+
+           
 root = tk.Tk()
+
+
 root.geometry("1440x1024")
 
 
@@ -20,7 +43,7 @@ position = tk.Label(root, text="Position", font=("Inter",24)).place(x=850,y=260 
 
 
 #buttons
-addImage_btn = customtkinter.CTkButton(root, text="Add Image", width=200 , height=40, corner_radius=40).place( y= 600, x=280)
+addImage_btn = customtkinter.CTkButton(root, text="Add Image", width=200 , height=40, corner_radius=40, command=choose_image).place( y= 600, x=280)
 saveImage_btn = customtkinter.CTkButton(root, text="Save Image", corner_radius=40, width=200, height=40,  fg_color="#F20E0E").place(x=880, y=630)
 modifyImage_btn= customtkinter.CTkButton(root, text="Modify Image", corner_radius=40, width=200, height=40).place(x=1130, y=630)
 enter_btn = customtkinter.CTkButton(root, text="Enter", width=200 , height=40, corner_radius=40).place(x=970, y= 200)
@@ -50,11 +73,10 @@ fontsize_scale= Scale( root, variable = font_size_value,
            orient = HORIZONTAL).place(x=860, y=540)
 
 #canvas
-canvas = tk.Canvas(root, width=700, height=500, bg="Grey").place(x=50,y=40)
-# original_image = Image.open(FILE_NAME)
+canvas = tk.Canvas(root, width=700, height=500, bg="Grey")
+canvas.place(x=50,y=40)
 # Optional: Resize the image
-# pil_image = original_image.resize((200, 150), Image.Resampling.LANCZOS)
-# tk_image = ImageTk.PhotoImage(original_image)
 # canvas.create_image(200, 150, image=tk_image, anchor=tk.CENTER)
+canvas.image = tk_image  # keep a reference!
 
 root.mainloop()
