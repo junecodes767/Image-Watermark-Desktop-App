50701087360d19779cede78e04197f6be1972a74
Priestest Alexander <priestest.alexander@gmail.com>
2025-09-24 07:20:43 -0400

    created a function to add the text to the image. BUt is not working as expected. I need to go over it and make adjustment. I think the problem has to do with the image being none at the time the image is needed I also need to look at the flow of the function to see if a elements needs a being function or line or code is being run before it has accquire to value it need to make sense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont,ImageTk
-from tkinter import Scale,HORIZONTAL
+from tkinter import Scale,HORIZONTAL,Toplevel,Canvas
 import tkinter as tk
 from tkinter import filedialog
 import customtkinter
@@ -9,7 +9,7 @@ original_image = None
 scaled_photo= None
 tk_image = None
 CANVAS_W, CANVAS_H = 700, 500 # canvas dimensions
-
+WATERMARK_TEXT = None
 
 def scale_image(img, max_w, max_h):
     """Return a scaled copy of img that fits inside max_w × max_h."""
@@ -36,10 +36,48 @@ def choose_image():
         tk_image = ImageTk.PhotoImage(scaled_photo)
         canvas.create_image(CANVAS_W/2, CANVAS_H/2, image=tk_image, anchor=tk.CENTER)
 
+def text_on_image():
+    """attaches the text unto the image as a watermark"""
+    global original_image,WATERMARK_TEXT
+    draw = ImageDraw.Draw(original_image)
+    font = ImageFont.truetype("arial.ttf", 40)
+    position = (50, 50) # Adjust coordinates as needed
+    fill_color = (255, 0, 0) # Red color
 
+    draw.text(position, WATERMARK_TEXT, font=font, fill=fill_color)
+    watermarked_image = original_image
+    scaled_photo =scale_image(watermarked_image,CANVAS_W,CANVAS_H)
+        
+    tk_image = ImageTk.PhotoImage(scaled_photo)
+    canvas.create_image(CANVAS_W/2, CANVAS_H/2, image=tk_image, anchor=tk.CENTER)
+    
+
+
+
+def image_or_text():
+    global radio_btn
+    if radio_btn.get() == "text":
+        open_new_window()
+    else:
+        pass
+    
+
+def open_new_window():
+    new_window = Toplevel(root)  # Create a new window
+    global WATERMARK_TEXT
+    new_window.title("Enter Text")
+    new_window.geometry("400x150")  
+    
+    customtkinter.CTkLabel(new_window, text="Enter Text to Attach To Image:", font=("Inter",20)).place(x=10,y=50)
+    customtkinter.CTkButton(new_window, text ="Enter", command =text_on_image).place(x=250,y=90)
+    watermark_text = tk.StringVar()
+    customtkinter.CTkEntry(new_window, textvariable=watermark_text,width=200).place(x=10,y=90)
+    WATERMARK_TEXT =watermark_text.get()
+     # close window on when the button is clicked
 
            
 root = tk.Tk()
+
 
 
 root.geometry("1440x1024")
@@ -60,12 +98,12 @@ position = tk.Label(root, text="Position", font=("Inter",24)).place(x=850,y=260 
 addImage_btn = customtkinter.CTkButton(root, text="Add Image", width=200 , height=40, corner_radius=40, command=choose_image).place( y= 600, x=280)
 saveImage_btn = customtkinter.CTkButton(root, text="Save Image", corner_radius=40, width=200, height=40,  fg_color="#F20E0E").place(x=880, y=630)
 modifyImage_btn= customtkinter.CTkButton(root, text="Modify Image", corner_radius=40, width=200, height=40).place(x=1130, y=630)
-enter_btn = customtkinter.CTkButton(root, text="Enter", width=200 , height=40, corner_radius=40).place(x=970, y= 200)
+enter_btn = customtkinter.CTkButton(root, text="Enter", width=200 , height=40, corner_radius=40,command=image_or_text).place(x=970, y= 200)
 
 #radio button
-my_var = tk.StringVar()
-watermark_text_input = tk.Radiobutton(root, text="Text", variable=my_var,font=("Inter",15)).place(x=850, y=100)
-watermark_image_input= tk.Radiobutton(root, text="Image", variable=my_var,font=("Inter",15)).place(x=1200, y=100)
+radio_btn = tk.StringVar()
+watermark_text_input = customtkinter.CTkRadioButton(root, text="Text", variable=radio_btn,font=("Inter",15),value ="text").place(x=850, y=100)
+watermark_image_input= customtkinter.CTkRadioButton(root, text="Image", variable=radio_btn,font=("Inter",15),value ="image").place(x=1200, y=100)
 
 x_var=tk.StringVar()
 
@@ -87,7 +125,7 @@ fontsize_scale= Scale( root, variable = font_size_value,
            orient = HORIZONTAL).place(x=860, y=540)
 
 #canvas
-canvas = tk.Canvas(root, width=CANVAS_W, height=CANVAS_H, bg="Grey")
+canvas = customtkinter.CTkCanvas(root, width=CANVAS_W, height=CANVAS_H, bg="Grey")
 canvas.place(x=50,y=40)
 # Optional: Resize the image
 # canvas.create_image(200, 150, image=tk_image, anchor=tk.CENTER)
